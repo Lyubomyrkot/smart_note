93318a20ec6489cdd38bfeca2d0a317e1b44d4c9
Lyubomyrkot <lyubomyrkot@gmail.com>
2024-11-23 18:24:37 +0200

    search

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from PyQt6.QtWidgets import QApplication
-from PyQt6.QtWidgets import QMainWindow, QMessageBox
+from PyQt6.QtWidgets import QMainWindow, QMessageBox, QListWidgetItem
 from ui import Ui_MainWindow
 from qt_material import apply_stylesheet
 import json
-
-
+from datetime import datetime
 
 class SmartNotes(QMainWindow):
     def   __init__(self):
@@ -17,6 +16,8 @@ class SmartNotes(QMainWindow):
         self.ui.save_btn.clicked.connect(self.save_note)
         self.ui.new_note.clicked.connect(self.new_note)
         self.ui.delete_btn.clicked.connect(self.del_note)
+        self.ui.pushButton.clicked.connect(self.search_note)
+        self.ui.lineEdit.textChanged.connect(self.search_note)
 
     def load_note(self):
         self.ui.note_list.clear()
@@ -27,11 +28,17 @@ class SmartNotes(QMainWindow):
             self.notes = {
                 "Нова нотатка": {
                     "text": "Тут буде ваші нотатки",
-                    "tags": [],
-                }
+                },
             }
+        self.show_list(self.notes)
 
-        self.ui.note_list.addItems(self.notes)
+    def show_list(self, notes):
+        for title, note in notes.items():
+            item = QListWidgetItem(title)
+            if "datetime" in note:
+                tip = f"Збережено: {note['datetime']}"
+                item.setToolTip(tip)
+            self.ui.note_list.addItem(item)
 
     def show_note(self):
         self.name = self.ui.note_list.selectedItems()[0].text()#отримуєм назву нотатки
@@ -44,13 +51,12 @@ class SmartNotes(QMainWindow):
         self.name = "Нова нотатка" 
 
     def del_note(self):
-        self.name = self.ui.note_list.selectedItems()[0].text
         if self.name in self.notes:
             del self.notes[self.name]
+            with open("notes.json", "w", encoding="utf-8") as file:
+                json.dump(self.notes,file, ensure_ascii=False)
+            self.load_note()
 
-        with open("notes.json", "w", encoding="utf-8") as file:
-            json.dump(self.notes,file, ensure_ascii=False)
-        self.load_note()
         self.new_note()
 
     def save_note(self):
@@ -65,6 +71,9 @@ class SmartNotes(QMainWindow):
             else:
                 self.notes[self.name]["text"] = self.ui.note_text.toPlainText()
             
+            now = datetime.now().strftime("%d.%m.%Y %H:%M")
+            self.notes[self.name]["datetime"] = now
+
             with open("notes.json", "w", encoding="utf-8") as file:
                 json.dump(self.notes,file, ensure_ascii=False)
             
@@ -76,7 +85,21 @@ class SmartNotes(QMainWindow):
             error_msg.setIcon(QMessageBox.warning)
             error_msg.exec()
 
-        
+    def search_note(self):
+        search = self.ui.lineEdit.text().strip().lower()
+        result = {}
+        if search != "":
+            title_list = self.notes.keys()
+            for title in title_list:
+                if search in title.lower():
+                    result[title] = self.notes[title]
+
+            self.ui.note_list.clear()
+            self.show_list(result)
+        else:
+            self.ui.note_list.clear()
+            self.show_list(self.notes)
+
 app = QApplication([])
 ex = SmartNotes()
 
